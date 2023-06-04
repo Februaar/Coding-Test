3bd6eb83abe16670c0ef008426cdd24615e3e9e5
Kyungjin Jung <kyoooungjin@gmail.com>
2023-06-05 01:24:20 +0900

    Update triangle.js
    인수 나열하는 조건 삭제

--- triangle.js
+++ triangle.js
@@ -2,7 +2,15 @@ function solution(a, b, c) {
   let answer = "";
 
   let sum = a + b + c;
-  let max = Math.max(a, b, c);
+  let max;
+
+  if (a > b && a > c) {
+    max = a;
+  } else if (b > a && b > c) {
+    max = b;
+  } else {
+    max = c;
+  }
 
   if (sum - max > max && sum - max <= c) {
     answer = "Yes";
